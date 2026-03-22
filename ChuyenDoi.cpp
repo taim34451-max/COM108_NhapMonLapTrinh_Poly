29f783a6b873ab89a157f59aff419d79cef861ad
Mai Thành Tài - PS47934 <taim34451@gmail.com>
2026-03-22 17:57:13 +0700

    Add language classification function
    Added a function to categorize programming languages and called it in main.

--- ChuyenDoi.cpp
+++ ChuyenDoi.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// Copy đoạn này lặp lại 10 lần để tăng dung lượng file
+void phan_loai_ngon_ngu() {
+    cout << "Toi dang hoc tai FPT Polytechnic" << endl;
+    cout << "Ngon ngu nay la C++" << endl;
+}
+
 int main() {
-    cout << "Nâng cấp lên C++ từ FPT Polytechnic!" << endl;
+    phan_loai_ngon_ngu();
     return 0;
 }
